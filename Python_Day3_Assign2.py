9bbb0e8260a6240850af6e4a1314adabf62e5b47
Rushikesh Shardul <mayureshshardul@gmail.com>
2024-08-28 10:39:51 +0530

    Add files via upload

---
+++ Python_Day3_Assign2.py
@@ -0,0 +1,9 @@
+# Taking two numbers as input from the user
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+# Swapping the numbers
+num1, num2 = num2, num1
+
+# Displaying the swapped values
+print(f"After swapping: First number is {num1}, Second number is {num2}")
